b42f587aff22e3163bf09a347d6fc91cc64c8fc2
ronimakifa <154334392+ronimakifa@users.noreply.github.com>
2024-01-30 13:07:19 +0200

    design

--- app/src/main/java/com/example/myapplication13/MainActivity.java
+++ app/src/main/java/com/example/myapplication13/MainActivity.java
@@ -3,12 +3,29 @@ package com.example.myapplication13;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button btn;
+    int num =0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn = findViewById(R.id.button);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(num%7 == 0){
+                    btn.setText("BOOM");
+
+                }
+            }
+        });
+
     }
 }
